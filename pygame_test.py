bf711702fc9eeed70f6b540c85c15cdb644c6ca6
mattfwilson <mattfwilson@gmail.com>
2022-03-01 11:22:02 -0500

    First commit of test pygame

---
+++ pygame_test.py
@@ -0,0 +1,25 @@
+# Importing the library
+import pygame
+  
+# Initializing Pygame
+pygame.init()
+  
+running = True
+while running == True:
+
+# Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
