d3f49a8f9749da5cb68ce397cd5d57a836d98e69
mattfwilson <mattfwilson@gmail.com>
2022-10-17 01:31:48 -0400

    Added player and enemy sprites and made it so the enemy can move/respawn.

--- platformer/scroller.py
+++ platformer/scroller.py
@@ -5,13 +5,19 @@ pg.init() # initializes pygame
 SCREEN = pg.display.set_mode((800, 512)) # creates main screen display dimensions
 CLOCK = pg.time.Clock() # creates pygame clock
 pg.display.set_caption('Platformer v1.0') # renames the title of the pygame window
-test_font = pg.font.Font(None, 30) # creates font surface (font-family, font-size)
+score_font = pg.font.Font('resources/font/PixelOperator-Bold.ttf', 30) # creates font surface (font-family, font-size)
 
-surf_color = pg.Surface((50, 50)) # create simple surface with color
-surf_color.fill('White') # create color for suface var
-surf_bg = pg.image.load('resources/bg.png') # create an image surface
-surf_ground = pg.image.load('resources/ground.png')
-surf_font = test_font.render('Score:', False, 'White') # creates font attributes (string, anti-aliasing, color)
+bg_surf = pg.image.load('resources/bg.png') # create an image surface
+ground_surf = pg.image.load('resources/ground.png')
+sloth_surf = pg.image.load('resources/enemy_sloth.png').convert_alpha() # creates font attributes (string, anti-aliasing, color)
+sloth_x_pos = 700
+
+score = 0
+score_surf = score_font.render('Score:', False, 'White') # creates font attributes (string, anti-aliasing, color)
+score_num_surf = score_font.render(str(score), False, 'White') # creates font attributes (string, anti-aliasing, color)
+
+player_surf = pg.image.load('resources/char.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 420))
 
 while True: # perpetual loop to keep the game running
     for event in pg.event.get(): # get all possible events in pg library
@@ -19,10 +25,15 @@ while True: # perpetual loop to keep the game running
             pg.quit() # quits out of pygame
             exit() # quits out of loop on system level (otherwise throws vid sys no initialized error)
     
-    SCREEN.blit(surf_color, (200, 100)) # blit = block image transfer (renders in call order)
-    SCREEN.blit(surf_bg, (0, 0)) # placement of image along x and y
-    SCREEN.blit(surf_ground, (0, 416))
-    SCREEN.blit(surf_font, (20, 18)) # placement of font along x and y
+    SCREEN.blit(bg_surf, (0, 0)) # placement of image along x and y
+    SCREEN.blit(ground_surf, (0, 416))
+    sloth_x_pos -= 3
+    if sloth_x_pos < -64:
+        sloth_x_pos = 800
+    SCREEN.blit(score_surf, (20, 18))
+    SCREEN.blit(score_num_surf, (115, 18))
+    SCREEN.blit(sloth_surf, (sloth_x_pos, 358))
+    SCREEN.blit(player_surf, player_rect)
     
     pg.display.update() # updates pg.display.set_mode()
     CLOCK.tick(60) # tells while loop to not run faster than 60fps 
