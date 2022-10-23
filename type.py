b1bd1e84fc18cb7f90892fb06f748f5e0abeadbe
mattfwilson <mattfwilson@gmail.com>
2022-10-23 00:02:52 -0400

    Resized window and started main logic for understaning key presses.

--- type.py
+++ type.py
@@ -1,7 +1,7 @@
-import pygame, sys
+import pygame, sys, keyboard
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 768))
+screen = pygame.display.set_mode((256, 256))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Typer')
 game_active = True
@@ -9,17 +9,25 @@ word = 'Hello'
 
 word_font = pygame.font.Font('platformer/resources/font/PixelOperator-Bold.ttf', 30)
 word_text = word_font.render(str(word), False, 'Black')
-word_rect = word_text.get_rect()
+word_rect = word_text.get_rect(center = (128, 128))
+
+for i in word:
+    print(i)
 
 while game_active:
     for event in pygame.event.get():
+        key = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('You pressed space')
+            else:
+                print(f'You pressed something else')
+    
     screen.fill(pygame.Color('White'))
     screen.blit(word_text, word_rect)
-    pygame.draw.rect(screen, 'Red', [50, 100, 50, 50], 0)
 
     pygame.display.update()
     clock.tick(60)
