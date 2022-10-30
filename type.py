845f3444d0d6cda4d5caf8d1162536d3760fba03
mattfwilson <mattfwilson@gmail.com>
2022-10-30 19:35:06 -0400

    Added detection for empty spaces.

--- type.py
+++ type.py
@@ -1,17 +1,24 @@
 import pygame, sys, keyboard
 
 pygame.init()
-screen = pygame.display.set_mode((256, 256))
+screen = pygame.display.set_mode((512, 256))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Typer')
 game_active = True
-word = 'Hello'
+word = 'Hello world. I am here.'
+spaces = 0
 
 word_font = pygame.font.Font('platformer/resources/font/PixelOperator-Bold.ttf', 30)
 word_text = word_font.render(str(word), False, 'Black')
-word_rect = word_text.get_rect(center = (128, 128))
+word_rect = word_text.get_rect(center = (256, 128))
 
 keys = {'space': pygame.K_SPACE, 'a': pygame.K_a, 'e': pygame.K_e}
+
+for i in word:
+    if i == ' ':
+        spaces += 1
+
+print(spaces)
 
 while game_active:
     for event in pygame.event.get():
