cb0c07f5a34ab27976ccafb84d2f5acba81a7229
mattfwilson <mattfwilson@gmail.com>
2022-10-31 20:58:21 -0400

    Trying to figure out how to read letters in string in sequence.

--- type.py
+++ type.py
@@ -5,8 +5,8 @@ screen = pygame.display.set_mode((512, 256))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Typer')
 game_active = True
-word = 'Hello world. I am here.'
-spaces = 0
+word = 'hello'
+mistakes = 0
 
 word_font = pygame.font.Font('platformer/resources/font/PixelOperator-Bold.ttf', 30)
 word_text = word_font.render(str(word), False, 'Black')
@@ -14,21 +14,17 @@ word_rect = word_text.get_rect(center = (256, 128))
 
 keys = {'space': pygame.K_SPACE, 'a': pygame.K_a, 'e': pygame.K_e}
 
-for i in word:
-    if i == ' ':
-        spaces += 1
-
-print(spaces)
+for letter in word:
+    if keyboard.is_pressed() == letter:
+        print(f'You pressed {letter}')
 
 while game_active:
     for event in pygame.event.get():
-        key = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        for i in word:
-            if keyboard.is_pressed(i):
-                print(i)
+            # if keyboard.is_pressed(i) == keyboard.read_key(i):
+            #     print(f'You pressed a letter in the word')
     
     screen.fill(pygame.Color('White'))
     screen.blit(word_text, word_rect)
