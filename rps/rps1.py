d43a51f6eb1d5864552e33d2f54154192d23c120
mattfwilson <mattfwilson@gmail.com>
2023-07-14 01:45:45 -0400

    Main app loop

--- rps/rps1.py
+++ rps/rps1.py
@@ -7,3 +7,11 @@ screen_height = 1000
 
 window = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('RPS Fight!')
+
+vel_x = 0
+vel_y = 0
+
+while running
+    for even in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
