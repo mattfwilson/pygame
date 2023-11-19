9ae82979cf421e04697124111dbf016657883a50
mattfwilson <mattfwilson@gmail.com>
2023-11-19 13:16:00 -0500

    Base template for pygame/pymunk

---
+++ template.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import pymunk as pm
+
+pg.init()
+
+display = pg.display.set_mode((800, 800))
+clock = pg.time.Clock()
+space = pm.Space()
+FPS = 60
+
+def game():
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
+        clock.tick(FPS)
+        space.step(1/FPS)
+
+game()
+pg.quit()
