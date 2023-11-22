9eb6b3053abefe640d5b14ab84ced6e2cfb4506f
mattfwilson <mattfwilson@gmail.com>
2023-11-22 02:28:22 -0500

    Boundaries set up and collision properly working!

--- rps_fight.py
+++ rps_fight.py
@@ -10,37 +10,69 @@ bg_color = (255, 255, 255)
 clock = pygame.time.Clock()
 space = pymunk.Space()
 FPS = 50
-radius = 10
+border_thickness = 10
 
-def convert_coordinates(point):
+
+bottom_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
+seg_bottom = pymunk.Segment(bottom_wall, (5, 5), (5, 595), border_thickness)
+seg_bottom.elasticity = 1
+space.add(seg_bottom, bottom_wall)
+
+left_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
+seg_left = pymunk.Segment(left_wall, (595, 5), (5, 5), border_thickness)
+seg_left.elasticity = 1
+space.add(seg_left, left_wall)
+
+top_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
+seg_top = pymunk.Segment(top_wall, (595, 595), (5, 595), border_thickness)
+seg_top.elasticity = 1
+space.add(seg_top, top_wall)
+
+right_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
+seg_right = pymunk.Segment(right_wall, (595, 595), (595, 5), border_thickness)
+seg_right.elasticity = 1
+space.add(seg_right, right_wall)
+
+def rand_spawn():
+    x = random.randint(50, 550)
+    y = random.randint(50, 550)
+    return x, y
+
+def convert_coords(point):
     return int(point[0]), 600 - int(point[1])
 
 class Ball():
     def __init__(self, x, y):
         self.body = pymunk.Body()
         self.body.position = x, y
-        self.shape = pymunk.Circle(self.body, radius)
+        self.shape = pymunk.Circle(self.body, 25)
         self.shape.elasticity = 1
         self.shape.density = 1
-        self.body.velocity = random.uniform(-400, 400), random.uniform(-400, 400)
+        self.body.velocity = random.uniform(300, 500), random.uniform(300, 500)
         space.add(self.body, self.shape)
 
     def draw(self):
-        pygame.draw.circle(display, (255, 0, 0), convert_coordinates(self.body.position), radius)
+        pygame.draw.circle(display, (255, 0, 0), convert_coords(self.body.position), 25)
         
 def game():
     ball1 = Ball(100, 100)
-    ball2 = Ball(100, 500)
-
-    handler = space.add_collision_handler()
+    ball2 = Ball(500, 100)
+    ball3 = Ball(100, 500)
+    ball4 = Ball(500, 500)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         display.fill(bg_color)
+        pygame.draw.line(display, (160, 160, 160), (5, 595), (595, 595), border_thickness) # bottom
+        pygame.draw.line(display, (160, 160, 160), (5, 595), (5, 5), border_thickness) # left
+        pygame.draw.line(display, (160, 160, 160), (5, 5), (595, 5), border_thickness) # top
+        pygame.draw.line(display, (160, 160, 160), (595, 595), (595, 5), border_thickness) # right
         ball1.draw()
         ball2.draw()
+        ball3.draw()
+        ball4.draw()
         pygame.display.update()
         clock.tick(FPS)
         space.step(1/FPS)
