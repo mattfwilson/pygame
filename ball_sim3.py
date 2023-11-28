ccb9e7d2f35192b2fe005b7bc9eb45b1518a5e87
mattfwilson <mattfwilson@gmail.com>
2023-11-28 00:16:13 -0500

    Expanded the dimensions to 1000x1000

--- ball_sim3.py
+++ ball_sim3.py
@@ -2,48 +2,46 @@ import pygame
 import pymunk
 import random
 
-running = True
-dimensions = (600, 600)
-bg_color = (0, 0, 0)
-wall_color = (255, 255, 255)
-FPS = 240
-border_thickness = 20
-collision_thickness = 10
-ball_count = 500
-ball_radius = 5
-ball_color = (255, 255, 255)
-count = 0
-
 pygame.init()
+running = True
+dimensions = (1000, 1000)
+FPS = 90
 display = pygame.display.set_mode(dimensions)
 clock = pygame.time.Clock()
 space = pymunk.Space()
 
+border_thickness = 20
+collision_thickness = 10
+ball_count = 2000
+ball_radius = 5
+color_black = (0, 0, 0)
+color_white = (255, 255, 255)
+color_red = (255, 0, 0)
+
 def draw_walls():
     bottom_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
-    seg_bottom = pymunk.Segment(bottom_wall, (5, 5), (5, 595), collision_thickness)
-    seg_bottom.elasticity = 1
-    space.add(seg_bottom, bottom_wall)
+    seg_left = pymunk.Segment(bottom_wall, (995, 995), (0, 995), collision_thickness)
+    seg_left.elasticity = 1
+    space.add(seg_left, bottom_wall)
+    pygame.draw.line(display, (color_white), (0, 995), (995, 995), border_thickness) # bottom
 
     left_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
-    seg_left = pymunk.Segment(left_wall, (595, 5), (0, 0), collision_thickness)
+    seg_left = pymunk.Segment(left_wall, (0, 0), (0, 995), collision_thickness)
     seg_left.elasticity = 1
     space.add(seg_left, left_wall)
+    pygame.draw.line(display, (color_white), (0, 995), (0, 0), border_thickness) # left
 
     top_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
-    seg_top = pymunk.Segment(top_wall, (595, 595), (5, 595), collision_thickness)
+    seg_top = pymunk.Segment(top_wall, (0, 0), (995, 0), collision_thickness)
     seg_top.elasticity = 1
     space.add(seg_top, top_wall)
+    pygame.draw.line(display, (color_white), (0, 0), (995, 0), border_thickness) # top
 
     right_wall = pymunk.Body(body_type=pymunk.Body.STATIC)
-    seg_right = pymunk.Segment(right_wall, (600, 600), (595, 5), collision_thickness)
+    seg_right = pymunk.Segment(right_wall, (995, 995), (995, 0), collision_thickness)
     seg_right.elasticity = 1
     space.add(seg_right, right_wall)
-
-    pygame.draw.line(display, (wall_color), (0, 600), (600, 600), border_thickness) # bottom
-    pygame.draw.line(display, (wall_color), (0, 600), (0, 0), border_thickness) # left
-    pygame.draw.line(display, (wall_color), (0, 0), (600, 0), border_thickness) # top
-    pygame.draw.line(display, (wall_color), (600, 600), (600, 0), border_thickness) # right
+    pygame.draw.line(display, (color_white), (995, 995), (995, 0), border_thickness) # right
 
 class Ball():
     def __init__(self, x, y, collision_type):
@@ -58,15 +56,19 @@ class Ball():
 
     def draw(self):
         if self.shape.collision_type != 2:
-            pygame.draw.circle(display, (ball_color), self.body.position, ball_radius)
+            pygame.draw.circle(display, color_white, self.body.position, ball_radius)
         else:
-            pygame.draw.circle(display, (255, 0, 0), self.body.position, 5)
+            pygame.draw.circle(display, color_red, self.body.position, ball_radius)
 
     def change_to_red(self, arbiter, space, data):
         self.shape.collision_type = 2
+        self.shape.elasticity += .01
+
+    def get_handler(self):
+        return self.shape.collision_type
 
 def rand_num():
-    return random.randint(dimensions[0] - 550, dimensions[1] - 50) 
+    return random.randint(dimensions[0] - 995, dimensions[1] - 50) 
 
 def collide(arbiter, space, data):
     print(f'Red ball collision')
@@ -79,13 +81,13 @@ def simulation():
     handlers = [space.add_collision_handler(2, i + 3) for i in range(ball_count)]
     for i, handler in enumerate(handlers):
         handler.separate = balls[i].change_to_red
-
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        display.fill(bg_color)
+        display.fill(color_black)
         draw_walls()
         [ball.draw() for ball in balls]
         pygame.display.set_caption('Bouncing Simulator')
