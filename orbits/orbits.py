b66e0eb8eee2576131d900d614c0b9f96fa02284
mattfwilson <mattfwilson@gmail.com>
2023-11-30 21:28:42 -0500

    Constants for Planet class

--- orbits/orbits.py
+++ orbits/orbits.py
@@ -10,6 +10,11 @@ color_black = (0, 0, 0)
 color_white = (255, 255, 255)
 
 class Planet:
+    ASTRO_UNITS = 149.6e6 * 1000
+    GRAVITY = 6.67428e-11
+    SCALE = 250 / ASTRO_UNITS # 1 astronomical units = 100 pixels
+    TIMESTEP = 3600 * 24 # represents 1 day
+    
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -18,6 +23,13 @@ class Planet:
         self.mass = mass
         self.x_vel = 0
         self.y_vel = 0
+        self.sun = False
+        self.dist_to_sun = 0
+        self.orbit = []
+
+    def draw(self, screen):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
 
 def simulation():
     running = True
@@ -25,7 +37,7 @@ def simulation():
 
     while running:
         clock.tick(60)
-        SCREEN.fill(color_white)
+        SCREEN.fill(color_black)
         pygame.display.update()
 
         for event in pygame.event.get():
