4105ecc4afe7f78bd901ee5259d28b47a8bca906
mattfwilson <mattfwilson@gmail.com>
2023-12-01 10:11:19 -0500

    Automatically pulling monitor dimensions and allowing quit via ESC key

--- orbits/orbits.py
+++ orbits/orbits.py
@@ -1,13 +1,21 @@
 import pygame
 import math
+from screeninfo import get_monitors
+import ctypes
 
 pygame.init()
 
-WIDTH, HEIGHT = 1000, 1000
+user = ctypes.windll.user32
+WIDTH = user.GetSystemMetrics(0)
+HEIGHT = user.GetSystemMetrics(1)
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Planet Simulation')
+
 color_black = (0, 0, 0)
 color_white = (255, 255, 255)
+color_yellow = (255, 255, 0)
+color_blue = (106, 187, 218)
+color_orange = (193, 108, 67)
 
 class Planet:
     ASTRO_UNITS = 149.6e6 * 1000
@@ -30,19 +38,35 @@ class Planet:
     def draw(self, screen):
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(screen, self.color, (x, y), self.radius)
 
 def simulation():
     running = True
     clock = pygame.time.Clock()
 
+    sun = Planet(0, 0, 30, color_yellow, 1.98892 * 10**30)
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.ASTRO_UNITS, 0, 16, color_blue, 5.9742 * 10**24 )
+    mars = Planet(-1.524 * Planet.ASTRO_UNITS, 0, 12, color_orange, 6.39 * 10**23)
+    planets = [sun, earth, mars]
+
     while running:
         clock.tick(60)
-        SCREEN.fill(color_black)
-        pygame.display.update()
+        
+        keys = pygame.key.get_pressed()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+        for planet in planets:
+            planet.draw(SCREEN)
+
+        pygame.display.update()
 
     pygame.quit()
 
